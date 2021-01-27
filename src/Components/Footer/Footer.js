541772a6e8c180824ce0b50a0dd209877d98dc19
김병진 <jin@gimbyeongjin-ui-MacBookPro.local>
2021-01-27 22:47:00 +0900

    Add: footer 구현 완료
    필요 없는 부분 삭제

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,39 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import Footertitle from "./Footertitle";
+import Footersubtitle from "./Footersubtitle";
+import styled from "styled-components";
 
-export default class Footer extends Component {
+class Footer extends Component {
   render() {
-    return <div></div>;
+    return (
+      <FooterWrap>
+        <Fooinner>
+          <List>
+            <Footertitle />
+          </List>
+          <List>
+            <Footersubtitle />
+          </List>
+        </Fooinner>
+      </FooterWrap>
+    );
   }
 }
+
+export default Footer;
+
+const FooterWrap = styled.footer`
+  position: relative;
+  background: #2c2a29;
+  height: 270px;
+`;
+
+const Fooinner = styled(FooterWrap)`
+  width: 1100px;
+  margin: 0 auto;
+  background-repeat: no-repeat;
+`;
+
+const List = styled.ul`
+  padding-top: 30px;
+`;
