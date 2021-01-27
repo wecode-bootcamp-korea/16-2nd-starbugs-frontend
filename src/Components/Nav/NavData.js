04b7e2c9de0ac4dfc5ba8954b59d53fc5800d9b4
김병진 <jin@gimbyeongjin-ui-MacBookPro.local>
2021-01-27 11:30:32 +0900

    Add: navbar 구현

---
+++ src/Components/Nav/NavData.js
@@ -0,0 +1,88 @@
+export const HEAD = [
+  {
+    id: 0,
+    content: "Sign In",
+    link: "/login",
+  },
+  {
+    id: 1,
+    content: "My Starbucks",
+    link: "/mypage",
+  },
+  {
+    id: 2,
+    content: "Customer Service",
+    link: "/service",
+  },
+  {
+    id: 3,
+    content: "Find a Store",
+    link: "/store",
+  },
+];
+
+export const HEAD1 = [
+  {
+    id: 0,
+    content: "커피",
+    link: "",
+  },
+  {
+    id: 1,
+    content: "스타벅스 원두",
+    link: "",
+  },
+  {
+    id: 2,
+    content: "스타벅스 비아",
+    link: "",
+  },
+  {
+    id: 3,
+    content: "스타벅스 오리가미",
+    link: "",
+  },
+];
+
+export const HEAD2 = [
+  {
+    id: 0,
+    content: "음료",
+    link: "/login",
+  },
+  {
+    id: 1,
+    content: "콜드 브루",
+    link: "/mypage",
+  },
+  {
+    id: 2,
+    content: "브루드 커피",
+    link: "/service",
+  },
+  {
+    id: 3,
+    content: "에스프레소",
+    link: "/store",
+  },
+  {
+    id: 4,
+    content: "에스프레소",
+    link: "/store",
+  },
+  {
+    id: 5,
+    content: "블랜디드",
+    link: "/store",
+  },
+  {
+    id: 6,
+    content: "스타벅스 피지오",
+    link: "/store",
+  },
+  {
+    id: 7,
+    content: "티(티바나)",
+    link: "/store",
+  },
+];
