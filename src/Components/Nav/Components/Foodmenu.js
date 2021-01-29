50ec37c7691a8660fac5e117bf8724cf2899b412
김병진 <jin@gimbyeongjin-ui-MacBookPro.local>
2021-01-30 00:40:13 +0900

    navbar 컴포넌트화 / map활용

---
+++ src/Components/Nav/Components/Foodmenu.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { HEAD2 } from "../NavData";
+
+export default class Foodmenu extends Component {
+  render() {
+    return (
+      <div className="gnb_sub_wrap">
+        <div className="gnb_sub">
+          <div className="gnb_sub_inner">
+            {HEAD2.map((menus) => {
+              return (
+                <ul className="gnb_sub_ul" key={menus.id}>
+                  {menus.title}
+                  {menus.content.map((menu, idx) => {
+                    return (
+                      <li
+                        key={menu[idx]}
+                        className={menu.id === 0 ? "" : "change"}
+                      >
+                        <Link to={menu.menu_link}>{menu.menu_title}</Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
